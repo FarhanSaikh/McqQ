a15de916c9dba1205af90730cd8f6995baba51da
FARHAN SAIKH <farhan.sk.587@gmail.com>
2018-03-11 03:34:34 +0530

    everything working timer and new layout updated

--- app/src/main/java/com/a2zdaddy/myquizapp/MainActivity.java
+++ app/src/main/java/com/a2zdaddy/myquizapp/MainActivity.java
@@ -1,6 +1,7 @@
 package com.a2zdaddy.myquizapp;
 
 import android.content.Intent;
+import android.os.CountDownTimer;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.util.Log;
@@ -15,6 +16,8 @@ import com.google.firebase.database.DatabaseReference;
 import com.google.firebase.database.FirebaseDatabase;
 import com.google.firebase.database.ValueEventListener;
 
+import java.util.Locale;
+
 public class MainActivity extends AppCompatActivity {
 
     DatabaseReference mQusetionRef,mStatousref;
@@ -24,6 +27,14 @@ public class MainActivity extends AppCompatActivity {
     private Button mButtonChoice2;
     private Button mButtonChoice3;
     private Button mButtonChoice4,quit;
+    private TextView mTextViewCountDown;
+    private static final long START_TIME_IN_MILLIS = 10000;
+    private CountDownTimer mCountDownTimer;
+    private long mTimeLeftInMillis = START_TIME_IN_MILLIS;
+     Button startbutton;
+
+
+
 
     private String qno;
     private String mAnswer;
@@ -34,8 +45,11 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-           quit=findViewById(R.id.quit);
+        mTextViewCountDown = findViewById(R.id.text_view_countdown);
+
+        quit=findViewById(R.id.quit);
            questioncountno=findViewById(R.id.qustioncount);
+           startbutton=findViewById(R.id.startbtn);
         mScoreView = (TextView)findViewById(R.id.score);
         mQuestionView = (TextView)findViewById(R.id.question);
         mButtonChoice1 = (Button)findViewById(R.id.choice1);
@@ -43,6 +57,13 @@ public class MainActivity extends AppCompatActivity {
         mButtonChoice3 = (Button)findViewById(R.id.choice3);
          mButtonChoice4=findViewById(R.id.choice4);
          updateQuestion();
+startbutton.setOnClickListener(new View.OnClickListener() {
+    @Override
+    public void onClick(View v) {
+        startTimer();
+        startbutton.setVisibility(View.GONE);
+    }
+});
 
          quit.setOnClickListener(new View.OnClickListener() {
              @Override
@@ -232,5 +253,42 @@ public class MainActivity extends AppCompatActivity {
     private void updateScore(int point) {
         mScoreView.setText("" + mScore);
     }
+
+
+    //timer
+    private void startTimer() {
+        mCountDownTimer = new CountDownTimer(mTimeLeftInMillis, 1000) {
+            @Override
+            public void onTick(long millisUntilFinished) {
+                mTimeLeftInMillis = millisUntilFinished;
+                updateCountDownText();
+            }
+
+            @Override
+            public void onFinish() {
+                Intent intent=new Intent(MainActivity.this,Main2Activity.class);
+                intent.putExtra("score",Integer.toString(mScore));
+                intent.putExtra("outof",qno);
+
+                startActivity(intent);
+                finish();
+
+
+
+            }
+        }.start();
+
+
+    }
+    private void updateCountDownText() {
+        int minutes = (int) (mTimeLeftInMillis / 1000) / 60;
+        int seconds = (int) (mTimeLeftInMillis / 1000) % 60;
+
+        String timeLeftFormatted = String.format(Locale.getDefault(), "%02d:%02d", minutes, seconds);
+
+        mTextViewCountDown.setText(timeLeftFormatted);
+    }
+
+
 }
 
